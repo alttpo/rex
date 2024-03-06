b012ebcf700f470de2fb3e0c06c15fb43c3b9914
jsd1982 <james.jdunne@gmail.com>
2024-03-05 18:20:39 -0600

    Add Catch2 testing library with initial test case

--- src/f64_test.cpp
+++ src/f64_test.cpp
@@ -1,46 +1,34 @@
 
 #include <vector>
+#include <catch2/catch_test_macros.hpp>
+#include <catch2/matchers/catch_matchers_range_equals.hpp>
 
 extern "C" {
 #include "f64dec.h"
 #include "f64enc.h"
 }
 
-int main() {
-    f64enc e;
-    enum f64enc_error err;
+TEST_CASE( "f64enc encodes", "f64enc" ) {
+    SECTION( "one byte frame" ) {
+        std::vector<u8> written;
+        f64enc e;
 
-    err = f64enc_init(
-        &e,
-        {
-            nullptr,
-            +[](void *ctx, unsigned len, const u8 *data) -> int {
-                for (int i = 0; i < len; i++) {
-                    printf(" %02X", *data++);
+        REQUIRE(
+            f64enc_init(
+                &e,
+                {
+                    &written,
+                    +[](void *ctx, unsigned len, const u8 *data) -> int {
+                        *(std::vector<u8> *)ctx = {data, data + len};
+                        return 0;
+                    }
                 }
-                printf("\n");
-                return 0;
-            }
-        }
-    );
-    if (err) {
-        fprintf(stderr, "%d\n", err);
-        return 1;
+            ) == F64ENC_ERR_SUCCESS
+        );
+
+        REQUIRE( f64enc_append_u8(&e, 1) == F64ENC_ERR_SUCCESS );
+        REQUIRE( f64enc_set_final(&e, true) == F64ENC_ERR_SUCCESS );
+        REQUIRE( f64enc_write(&e) == F64ENC_ERR_SUCCESS );
+        REQUIRE_THAT(written, Catch::Matchers::RangeEquals(std::vector<u8>{ (u8)0x81, (u8)0x01 }));
     }
-
-    err = f64enc_append_u8(&e, 1);
-    if (err) {
-        fprintf(stderr, "%d\n", err);
-        return 1;
-    }
-
-    f64enc_set_final(&e, true);
-
-    err = f64enc_write(&e);
-    if (err) {
-        fprintf(stderr, "%d\n", err);
-        return 1;
-    }
-
-    return 0;
 }
