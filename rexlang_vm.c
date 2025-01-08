763376bccea17313aa8742e7a23371ba87167e25
jsd1982 <james.jdunne@gmail.com>
2025-01-07 23:47:16 -0600

    Small start on VM implementation

--- rexlang_vm.c
+++ rexlang_vm.c
@@ -3,12 +3,16 @@
 #include <stdbool.h>
 #include "rexlang_vm.h"
 
-void rex_vm_init(struct rexlang_vm *vm)
+void rexlang_init(struct rexlang_vm *vm)
 {
+	vm->ip = REXLANG_MEMOFFS_PRGM;
+	vm->sp = sizeof(vm->s);
 	memset(vm->m, 0, sizeof(vm->m));
+	memset(vm->s, 0, sizeof(vm->s));
+	memset(vm->st, 0, sizeof(vm->st));
 }
 
-void rex_vm_exec(struct rexlang_vm *vm)
+void rexlang_exec(struct rexlang_vm *vm)
 {
 	
 }
--- rexlang_vm.h
+++ rexlang_vm.h
@@ -4,54 +4,32 @@
 
 #include <stdint.h>
 
-#ifndef REXLANG_MEMSZ
-#  define REXLANG_MEMSZ 1024
+#ifndef REXLANG_MEMSZ_PROG
+#  define REXLANG_MEMSZ_PROG 3172
+#endif
+#ifndef REXLANG_MEMSZ_DATA
+#  define REXLANG_MEMSZ_DATA 1024
 #endif
 
-#if REXLANG_MEMSZ > 4096
-#  error REXLANG_MEMSZ cannot be more than 4096 bytes
+#if REXLANG_MEMSZ_PROG + REXLANG_MEMSZ_DATA > 65536
+#  error REXLANG_MEMSZ_PROG + REXLANG_MEMSZ_DATA cannot exceed 65336 bytes
 #endif
+
+#define REXLANG_MEMOFFS_PRGM REXLANG_MEMSZ_DATA
 
 struct rexlang_vm {
-	uint16_t   p;               // program counter as index into m[], starts at 0
-	uint16_t   s;               // stack pointer as index into m[], starts at len(m)-1
-	uint16_t   d;               // data allocation pointer
+	uint16_t ip;    // program counter as index into m[], starts at REXLANG_MEMOFFS_PRGM
+	uint16_t sp;    // stack pointer as index into s[], starts at sizeof(s)
 
-	// TODO: store multiple contexts in m[] and denote split points
+	// data+program memory:
+	uint8_t  m[REXLANG_MEMSZ_DATA+REXLANG_MEMSZ_PROG];
 
-	uint8_t m[REXLANG_MEMSZ];    // read-write memory
+	// stack values and their types stored in bitfield (0 = u8, 1 = u16):
+	uint8_t  s[64]; // stack
+	uint32_t st[2]; // stack type bits
 };
 
+void rexlang_init(struct rexlang_vm *vm);
 void rexlang_exec(struct rexlang_vm *vm);
-
-enum rexlang_op {
-	REXLANG_OP_RETURN = 0,
-	REXLANG_OP_GOTO,
-	REXLANG_OP_IF,
-
-	// TBD...
-
-	REXLANG_OP_NOT,
-	REXLANG_OP_AND,
-	REXLANG_OP_OR,
-	REXLANG_OP_XOR,
-	REXLANG_OP_SHL,
-	REXLANG_OP_SHR,
-	REXLANG_OP_EQ,
-	REXLANG_OP_NE,
-
-	REXLANG_OP_ADD,
-	REXLANG_OP_SUB,
-
-	REXLANG_OP_GT_SIGNED,
-	REXLANG_OP_LT_SIGNED,
-	REXLANG_OP_GE_SIGNED,
-	REXLANG_OP_LE_SIGNED,
-
-	REXLANG_OP_GT_UNSIGNED,
-	REXLANG_OP_LT_UNSIGNED,
-	REXLANG_OP_GE_UNSIGNED,
-	REXLANG_OP_LE_UNSIGNED,
-};
 
 #endif
