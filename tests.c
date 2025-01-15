97b88611efb6edc09d81342fdc7b977eb2c6ba23
jsd1982 <james.jdunne@gmail.com>
2025-01-14 23:26:32 -0600

    tests: Added expect() macro with convenient string formatting

--- tests.c
+++ tests.c
@@ -2,6 +2,7 @@
 #include <stddef.h>
 #include <stdint.h>
 #include <stdio.h>
+#include <string.h>
 #include "rexlang_vm.h"
 #include "rexlang_vm_impl.h"
 
@@ -33,13 +34,23 @@ struct test_t {
     int         check_stack_count;      // number of stack items to check
     uint32_t    check_stack_values[4];  // values of stack items to check
 
-    int (*check_fn)(struct rexlang_vm *vm);
+    int (*check_fn)(struct rexlang_vm* vm, char* msg);
 };
 
-int test_chip_set_addr(struct rexlang_vm *vm) {
-    if (chip_addr[0x3F] != 0x2C00) {
-        return 2;
-    }
+#define VALUE_TO_STRING(x) #x
+#define VALUE(x) VALUE_TO_STRING(x)
+#define expect(expected_expr, actual_expr, msg_out) { \
+    uint32_t expected = expected_expr; \
+    uint32_t actual = actual_expr; \
+    if (actual != expected) { \
+        sprintf((msg_out), " expected %08X, got %08X", expected, actual); \
+        return 2; \
+    } \
+}
+
+int test_chip_set_addr(struct rexlang_vm* vm, char* msg) {
+    int i = sprintf(msg, "chip_addr[0x3F]");
+    expect(0x2C00, chip_addr[0x3F], msg+i);
 
     return 0;
 }
@@ -60,9 +71,22 @@ const struct test_t tests[] = {
         },
         test_chip_set_addr,
     },
+    {
+        "halt",
+        {
+            0,                  // halt
+            0b01000000, 0x3F    // push-u8 0x3F
+        },
+        REXLANG_ERR_HALTED,
+        1,
+        {
+            0   // expect the push-u8 to not be executed after the halt
+        },
+        NULL,
+    },
 };
 
-int exec_test(const struct test_t *t) {
+int exec_test(const struct test_t *t, char* msg) {
     enum rexlang_error err;
     struct rexlang_vm vm;
     uint8_t data[256] = {0};
@@ -74,8 +98,13 @@ int exec_test(const struct test_t *t) {
         return 1;
     }
 
+    for (int i = 0; i < t->check_stack_count; i++) {
+        int n = sprintf(msg, "stack[%d]", i);
+        expect(t->check_stack_values[i], vm.ki[REXLANG_STACKSZ - i - 1], msg+n);
+    }
+
     if (t->check_fn) {
-        int ret = t->check_fn(&vm);
+        int ret = t->check_fn(&vm, msg);
         if (ret) {
             return ret;
         }
@@ -85,11 +114,13 @@ int exec_test(const struct test_t *t) {
 }
 
 int main(void) {
+    char msg[256] = {0};
+
     for (int i = 0; i < sizeof(tests)/sizeof(struct test_t); i++) {
         printf("executing test: %s\n", tests[i].name);
-        int ret = exec_test(&tests[i]);
+        int ret = exec_test(&tests[i], msg);
         if (ret) {
-            printf("** test FAILED! ret=%d\n", ret);
+            printf("** test FAILED! (%d); %s\n", ret, msg);
             return 1;
         }
     }
